836720ef6b36c3bd00f5b63b9e005b51d23aa6a0
ANTONanton1984451 <anton.shushp@yandex.ru>
2021-01-14 18:10:57 +0300

    added update method  for blog

--- App/Http/Controllers/BlogController.php
+++ App/Http/Controllers/BlogController.php
@@ -10,26 +10,8 @@ class BlogController extends Controller
 {
     public function create()
     {
-        $columns = [
-            'href' => $this->request->getQuery('href'),
-            'title' => $this->request->getQuery('title'),
-            'body' =>  $this->request->getQuery('body'),
-            'description' => $this->request->getQuery('description'),
-            'product' => $this->request->getQuery('product'),
-            'view' => $this->request->getQuery('view'),
-            'time_create' => $this->request->getQuery('time_create')
-        ];
-
-
-        $blog = new Blog();
-
-        foreach ($columns as $column => $value)
-        {
-            $blog->$column = $value;
-        }
-
-        $blog->save();
-
+        $blogInstance = Blog::findByKey($this->request->getQuery('href'));
+        $blogInstance->updateBody($this->request->getQuery('body'));
         return response('','json');
     }
 }
--- App/Models/Blog.php
+++ App/Models/Blog.php
@@ -8,6 +8,12 @@ class Blog extends Model
 {
     protected string $primaryKey = 'href';
 
+    public function updateBody(string $string)
+    {
+        $statement = $this->connection->prepare("update $this->tableName set body = ? where $this->primaryKey = ?");
+        $statement->execute([$string,$this->columns[$this->primaryKey]]);
+
+    }
 
 
 }
